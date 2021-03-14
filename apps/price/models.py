8e1abe5c32acf6728e8d4cb809a85d2027c470a3
edzen12 <oichiev.edzen@gmail.com>
2021-03-14 15:47:03 +0600

    edit files

--- apps/price/models.py
+++ apps/price/models.py
@@ -4,7 +4,6 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 class Price(models.Model):
     title = models.CharField(verbose_name="Название тарифа", max_length=50)
-    image = models.FileField(verbose_name="фото", upload_to='price/', blank=True, null=True)
     sub_title = models.CharField(verbose_name="Под название", max_length=50)
     price = models.FloatField(verbose_name="Цена")
     desc_list = RichTextUploadingField(verbose_name="Описание услуги")
